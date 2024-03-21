470e63c8ef60171c53d1e1ed310941c672f221f5
Jai Krishna <102488496+KnightJai@users.noreply.github.com>
2024-03-22 03:04:25 +0530

    registering user

--- src/app.js
+++ src/app.js
@@ -28,6 +28,12 @@ app.search(cookieParser())
 
 import userRouter from "./routes/user.routes.js"
 
+//routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 
-export {app}
+
+export {app, userRouter}
