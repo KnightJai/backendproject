f883d095f5539b05a17c084f953c99b5dda761d9
Jai Krishna <102488496+KnightJai@users.noreply.github.com>
2024-03-21 13:23:40 +0530

    app js changes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
+
+
+const app= express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS-ORIGIN,
     credentials: true
+
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.jspn({limit: "16kb"}))
+
+app.use(express.urlencoded({extended:true, limit: "16kb"}))
+
 app.use(express.static("public"))
-app.use(cookieParser())
+app.search(cookieParser())
+
+
+export {app}
